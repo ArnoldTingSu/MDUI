a6f036ceae2d107aaf9a4a1cc80d38bc34b0e1f5
Arnold Su <arnoldtingsu@gmail.com>
2023-11-25 23:47:47 -0800

    added dialog function with animation 'fullscreen'

--- vuetify-dashboard/src/views/Posts.vue
+++ vuetify-dashboard/src/views/Posts.vue
@@ -53,35 +53,73 @@ const search = ref("")
 </script>
 
 <template>
-    <div>
-        <h1>POSTS!</h1>
-        <v-text-field
+<div>
+    <h1>POSTS!</h1>
+    <v-text-field
         v-model="search"
         label="Search"
         prepend-inner-icon="mdi-magnify"
         single-line
         variant="outlined"
         hide-details
-      ></v-text-field>
-        <v-data-table 
-            :items="posts"
-            :headers="[
-                {
-                    title: 'Post Title',
-                    align:'start',
-                    sortable: false,
-                    key: 'title'
-                },
-                {
-                    title: 'Author',
-                    align: 'end',
-                    key: 'author'
-                }
-            ]"
-            show-select
-            item-value="title"
-            v-model="selected"
-            :search="search"
-        ></v-data-table>
-    </div>
+    ></v-text-field>
+    <v-data-table 
+        :items="posts"
+        :headers="[
+            {
+                title: 'Post Title',
+                align:'start',
+                sortable: false,
+                key: 'title'
+            },
+            {
+                title: 'Author',
+                align: 'end',
+                key: 'author'
+            }
+        ]"
+        show-select
+        item-value="title"
+        v-model="selected"
+        :search="search"
+    >
+    <template #item.title="{ item }">
+    <v-dialog
+        fullscreen
+    >
+        <template v-slot:activator="{ props }">
+        <button
+            v-bind="props"
+        >
+            {{item.title}}
+        </button>
+        </template>
+    <template v-slot:default="{ isActive }">
+        <v-card>
+        <v-card-title class="text-h5 grey lighten-2">
+            Privacy Policy
+        </v-card-title>
+
+        <v-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <button
+                color="primary"
+                text
+                @click="isActive.value = false"
+                >
+                I accept
+            </button>
+        </v-card-actions>
+        </v-card>     
+    </template>
+    </v-dialog>
+    </template>
+    </v-data-table>
+</div>
 </template>
